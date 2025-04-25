f40d6d91408f3f61a12194fc6af513d4278710ae
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-04-25 21:58:04 +0500

    feat: Add constants for game colors and timing in game module

--- src/game.rs
+++ src/game.rs
@@ -2,3 +2,10 @@ use piston_window::{*,types::Color};
 use rand::{thread_rng, Rng};
 use crate::snake::{Direction, Snake};
 use crate::draw::{draw_block, draw_rectangle};
+
+const FOOD_COLOR: Color = [0.80,0.00,0.00,1.0];
+const BORDER_COLOR: Color = [0.00,0.00,0.00,1.0];
+const GAMEOVER_COLOR: Color = [0.90,0.00,0.00,0.5];
+
+const MOVING_PERIOD: f64 = 0.1;
+const RESTART_TIME: f64 = 1.0;
