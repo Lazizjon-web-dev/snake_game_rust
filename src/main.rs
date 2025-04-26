7041109d910caf74ab91177af406421e58b298fa
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-04-26 21:45:48 +0500

    refactor: Add lazy loading for images in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate piston_window;
+extern crate rand;
+
+mod draw;
+mod game;
+mod snake;
+
+use draw::to_coord_u32;
+use game::Game;
+use piston_window::{types::Color, *};
 fn main() {
     println!("Hello, world!");
 }
