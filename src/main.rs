3ab8b36c5a0be648d7f82f870e5caa3ec248b9a2
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-04-26 21:46:43 +0500

    refactor: Update main.rs to include background color constant and adjust game dimensions

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,11 @@ mod snake;
 use draw::to_coord_u32;
 use game::Game;
 use piston_window::{types::Color, *};
+
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
 fn main() {
+    let (width, height) = (20, 20);
+
     println!("Hello, world!");
 }
